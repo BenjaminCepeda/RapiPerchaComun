f5716d4165464102ef9fe219378063baf8c75070
Benjamin Cepeda <bcepeda_98@yahoo.com>
2020-12-11 18:02:51 -0500

    Mensajes genericos

---
+++ test/ec/com/kodice/rapipercha/comun/test/Principal.java
@@ -0,0 +1,24 @@
+package ec.com.kodice.rapipercha.comun.test;
+
+import ec.com.kodice.rapipercha.util.UtilPresentacion;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Esta clase contiene los atributos y metodos de un Principal
+ * @author Benjamin Cepeda
+ * @version v1.0
+ * @date 2020/12/10 
+ */
+public class Principal {
+    
+    public static void main(String[] args) {
+        //TODO 
+        UtilPresentacion.mostrarMensajeInformativo(null, "este es un mensaje de prueba");
+   }    
+
+}
